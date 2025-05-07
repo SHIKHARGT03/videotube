112cddd7f57944cfd6d4f6ee2c5ec8f88b693fa2
SHIKHARGT03 <shikhargt0308@gmail.com>
2025-05-07 14:16:00 +0530

    Commit all recent changes

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+import {app} from './app.js'
+
+dotenv.config({
+    path: './.env'
+})
+
+const startServer = async () => {
+    try {
+        await connectDB()
+        const port = process.env.PORT || 8000
+        app.listen(port, () => {
+            console.log(`⚙️ Server is running at port : ${port}`);
+        })
+    } catch (error) {
+        console.log("ERROR: ", error);
+        process.exit(1)
+    }
+}
+
+startServer()
